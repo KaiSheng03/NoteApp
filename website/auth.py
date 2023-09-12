f30d111a0f122339d5ab8a40ad02f56bbc54bd44
KaiSheng03 <mankaisheng3@gmail.com>
2023-09-12 12:51:07 +0800

    changes

--- website/auth.py
+++ website/auth.py
@@ -32,59 +32,38 @@ def logout():
 
 @auth.route('/signup', methods=['GET','POST'])
 def signup():
-    while True:
-        if request.method == 'POST':
-            email = request.form.get('email')
-            firstName = request.form.get('firstName')
-            password1 = request.form.get('password')
-            password2 = request.form.get('password2')
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password')
+        password2 = request.form.get('password2')
 
-            user = User.query.filter_by(email=email).first()
-            if user:
-                flash("Email already exists", category='error')
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash("Email already exists", category='error')
 
-            elif len(email)==0:
-                flash("Please enter an email", category='error')
+        elif len(email)==0:
+            flash("Please enter an email", category='error')
             
-            elif len(firstName)==0:
-                flash("Please enter your first name", category='error')
+        elif len(firstName)==0:
+            flash("Please enter your first name", category='error')
             
-            elif len(password1)==0:
-                flash("Please enter a password", category='error')
+        elif len(password1)==0:
+            flash("Please enter a password", category='error')
 
-            # Password must be at least 8 characters long
-            elif len(password1) < 8:
-                flash("Password must have at least 8 characters long", category='error')
-        
-            # Password must contain at least one uppercase letter
-            elif not any(char.isupper() for char in password1):
-                flash("Password must contain at least one uppercase letter", category='error')
-        
-            # Password must contain at least one lowercase letter
-            elif not any(char.islower() for char in password1):
-                flash("Password must contain at least one lowercase letter", category='error')
-            
-            # Password must contain at least one digit
-            elif not any(char.isdigit() for char in password1):
-                flash("Password must contain at least one digit", category='error')
+        elif len(password2)==0:
+            flash("Please confirm your password", category='error')
 
-            # Password must contain at least one special character
-            elif not any(char in "!@#$%^&*()_+{}[]|\"'<>,.?/~" for char in password1):
-                flash("Password must contain at least one special character", category='error')     
+        elif password1 != password2:
+            flash("The passwords don't match", category='error')
 
-            elif len(password2)==0:
-                flash("Please confirm your password", category='error')
-
-            elif password1 != password2:
-                flash("The passwords don't match", category='error')
-
-            else:
-                new_user = User(email=email, first_name=firstName, password=generate_password_hash(password1, method='sha256'))
-                db.session.add(new_user)
-                db.session.commit()
-                login_user(new_user, remember=True)
-                flash("Account created", category='success')
-                return redirect(url_for('views.home'))
+        else:
+            new_user = User(email=email, first_name=firstName, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
+            flash("Account created", category='success')
+            return redirect(url_for('views.home'))
         
     return render_template("sign_up.html", user=current_user)    
 
@@ -97,10 +76,13 @@ def renew():
         
         user = User.query.filter_by(email=email).first()
         if user:
-            user.password = generate_password_hash(newPassword1, method='sha256')
-            db.session.commit()
-            flash("Password updated successfully", category='success')
-            return redirect(url_for('auth.login'))
+            if newPassword1 == newPassword2:
+                user.password = generate_password_hash(newPassword1, method='sha256')
+                db.session.commit()
+                flash("Password updated successfully", category='success')
+                return redirect(url_for('auth.login'))
+            else:
+                flash("The passwords don't match", category='error')
 
         else:
             flash("Cannot find user", category='error')
--- website/static/script.js
+++ website/static/script.js
@@ -30,3 +30,34 @@ function setEnabled(){
     address.disabled = false;
     btnUpdate.disabled = false;
 }
+
+function passwordValidate(){
+    password = document.getElementById('password').value;
+    errorBox = document.getElementById('errorBox');
+    const minLength = 8;
+    const hasNumber = /\d/.test(password);
+    const hasSpecialCharacter = /[!@#$%^&*]/.test(password);
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+
+    if(password.length < minLength){
+        errorBox.textContent = "Password must have at least 8 characters long";
+    }
+    else if(!hasNumber){
+        errorBox.textContent = "Password must contain at least one digit";
+    }
+    else if(!hasSpecialCharacter){
+        errorBox.textContent = "Password must contain at least one special character";
+    }
+    else if(!hasUpperCase){
+        errorBox.textContent = "Password must contain at least one uppercase letter";
+    }
+    else if(!hasLowerCase){
+        errorBox.textContent = "Password must contain at least one lowercase letter";
+    }
+    else{
+        errorBox.textContent = "";
+        btnSignUp = document.getElementById('btnSignUp');
+        btnSignUp.disabled = false;
+    }
+}
