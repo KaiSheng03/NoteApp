21382de3e22d9d6a99db08725155bcb8b73c7b49
KaiSheng03 <mankaisheng3@gmail.com>
2023-09-25 20:16:51 +0800

    changes

--- website/auth.py
+++ website/auth.py
@@ -1,7 +1,5 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
-from flask_pymongo import PyMongo
-from . import mongo
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, login_required, logout_user, current_user
 from bson import ObjectId
